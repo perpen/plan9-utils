171e346c1f3127b0997118c21b8d8ff53187e237
Henri Ducrocq <henri.ducrocq@gmail.com>
2023-02-14 22:37:32 +0000

    x

--- acmeevent2/acmeevent2.c
+++ acmeevent2/acmeevent2.c
@@ -1,11 +1,16 @@
 #include <u.h>
 #include <libc.h>
+#include <fcall.h>
+#include <9pclient.h>
+#include <auth.h>
 #include <thread.h>
 #include <stdio.h>
 #include <bio.h>
 
+
 char *winid;
 Channel *chan;
+CFsys *fs;
 
 int
 getn(Biobuf *b)
@@ -69,34 +74,35 @@ getevent(Biobuf *b, int *c1, int *c2, int *q0, int *q1, int *flag, int *nr, char
 void
 eventer()
 {
-	int c1, c2, q0, q1, eq0, eq1, flag, nr, x, eventpathlen;
-	Biobuf *b;
-	char buf[2000], buf2[2000], buf3[2000], eventpath[1000], msg[8000];
+	int c1, c2, q0, q1, eq0, eq1, flag, nr, x, fd;
+	Biobuf b;
+	char buf[2000], buf2[2000], buf3[2000], path[1000], msg[8000];
 
-	eventpathlen = snprintf(eventpath, sizeof(eventpath), "/mnt/acme/%s/event", winid);
-	assert(eventpathlen < sizeof(eventpath));
-	b = Bopen(eventpath, OREAD);
-	assert(b != nil);
-
+	assert(snprint(path, sizeof(path), "%s/event", winid) < sizeof(path));
+    fd = fsopenfd(fs, path, OREAD);
+    if(fd < 0){
+        sysfatal("acmedot: cannot open %s: %r", buf);
+    }
+	assert(Binit(&b, fd, OREAD) >= 0);
 	for(;;){
-		getevent(b, &c1, &c2, &q0, &q1, &flag, &nr, buf);
+		getevent(&b, &c1, &c2, &q0, &q1, &flag, &nr, buf);
 		eq0 = q0;
 		eq1 = q1;
 		buf2[0] = 0;
 		buf3[0] = 0;
 		if(flag & 2){
 			/* null string with non-null expansion */
-			getevent(b, &x, &x, &eq0, &eq1, &x, &nr, buf);
+			getevent(&b, &x, &x, &eq0, &eq1, &x, &nr, buf);
 		}
 		if(flag & 8){
 			/* chorded argument */
-			getevent(b, &x, &x, &x, &x, &x, &x, buf2);
-			getevent(b, &x, &x, &x, &x, &x, &x, buf3);
+			getevent(&b, &x, &x, &x, &x, &x, &x, buf2);
+			getevent(&b, &x, &x, &x, &x, &x, &x, buf3);
 		}
 		snprint(msg, sizeof(msg),
 			"handleevent %c %c %d %d %d %d %d %d %q %q %q",
 			c1, c2, q0, q1, eq0, eq1, flag, nr, buf, buf2, buf3);
-		if(c1 != 'F') // probably our use of the acme files
+		if(c1 != 'F') // F: our use of the acme files
 			sendp(chan, msg);
 	}
 }
@@ -104,16 +110,20 @@ eventer()
 void
 logger()
 {
-	Biobuf *b;
+	Biobuf b;
 	char *line;
 	char msg[1000], id[10], op[20], name[1000];
+	int fd;
 
-	b = Bopen("/mnt/acme/log", OREAD);
-	assert(b != nil);
+    fd = fsopenfd(fs, "log", OREAD);
+    if(fd < 0){
+        sysfatal("acmedot: cannot open log: %r");
+    }
+	assert(Binit(&b, fd, OREAD) >= 0);
 	for(;;){
-		line = Brdline(b, '\n');
+		line = Brdline(&b, '\n');
 		assert(line != 0);
-		line[Blinelen(b)] = '\0';
+		line[Blinelen(&b)] = '\0';
 		name[0] = '\0';
 		sscanf(line, "%s %s %s", id, op, name);
 		snprint(msg, sizeof(msg), "handlelog %s %s %q\n", id, op, name);
@@ -125,7 +135,7 @@ static void
 usage(void)
 {
 	fprint(2, "usage: %s [-l]\n", argv0);
-	exits("usage");
+	threadexitsall("usage");
 }
 
 void
@@ -146,12 +156,18 @@ threadmain(int argc, char **argv)
 		usage();
 	}ARGEND
 
+	fs = nsmount("acme", nil);
+	if(fs == nil){
+		sysfatal("acmedot: %r");
+	}
+
 	chan = chancreate(sizeof(char*), 0);
 	if(dolog)
 		proccreate(logger, nil, 16*1024);
 	proccreate(eventer, nil, 16*1024);
 	for(;;){
 		msg = recvp(chan);
+		// fprintf(stderr, "printing: %s\n", msg);
 		print("%s\n", msg);
 	}
 }
